15e574fed425bb36a5a8594ff352e088a74e2ae8
Ethan <ethan.chen@covenant.edu>
2020-02-12 21:13:12 -0500

    Saving with Pickle and Graphing with Matplotlib

--- tensorEnv/Regression Working File.py
+++ tensorEnv/Regression Working File.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
 from sklearn.utils import shuffle
 
 # Read csv
@@ -28,37 +32,58 @@ X = np.array(data.drop([predict], 1))
 # Save G3 into another numpy array
 y = np.array(data[predict])
 
-# Split into 4 variables
-# x test
-# y test
-# x train
-# y train
-
-# Create 4 different arrays
-# x_train will be a section of X's array
-# y_train will be a section of y's array
-# x_test and y_test are going to be the test data that we are going to use
-# to test the accuracy of our model or algorithm
-# The question is Why are we splitting the data?
-# If we train the model with every single bit of data that we have, then
-# it will simply just memorize patterns
-# test_size = 0.1 means that we are splitting up 10% of the data into test samples
-# so that when we test we can test off that
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
 
-# Now, we need to create a learning model
-linear = linear_model.LinearRegression()
+'''
+# We're going to run the test 30 times to get the best accuracy rate and
+# then save that one
+best = 0
+for _ in range(30):
+    # Split into 4 variables
+    # x test
+    # y test
+    # x train
+    # y train
 
-# Fit this data to find the best fit line. Refer to image 'LinearRegression_IMG' for
-# brief explanation
-linear.fit(x_train, y_train)
+    # Create 4 different arrays
+    # x_train will be a section of X's array
+    # y_train will be a section of y's array
+    # x_test and y_test are going to be the test data that we are going to use
+    # to test the accuracy of our model or algorithm
+    # The question is Why are we splitting the data?
+    # If we train the model with every single bit of data that we have, then
+    # it will simply just memorize patterns
+    # test_size = 0.1 means that we are splitting up 10% of the data into test samples
+    # so that when we test we can test off that
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-# Accuracy of the model
-accuracy = linear.score(x_test, y_test);
+    # Now, we need to create a learning model
+    linear = linear_model.LinearRegression()
 
-# Print the accuracy
-print(accuracy)
+    # Fit this data to find the best fit line. Refer to image 'LinearRegression_IMG' for
+    # brief explanation
+    linear.fit(x_train, y_train)
+
+    # Accuracy of the model
+    accuracy = linear.score(x_test, y_test);
+
+    # Print the accuracy
+    print(accuracy)
+
+    # Only save the model for the best accuracy
+    if accuracy > best:
+        best = accuracy
+        # Save a pickle file in our directory
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)'''
+
+
+
+
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 # Output: ~ 0.8363248776025236 => 84% Accuracy. This model is okay.
 # Obviously, we could do better... but for determining student's grades;
@@ -84,3 +109,16 @@ for x in range(len(predictions)):
 # Explanation for example output:
 # prediction            [G1, G2, studytime, failures, absences]             Actual final grade
 # 10.479946612966435                [13 10  2  1 22]                               11
+
+# Select style for the plot
+style.use("ggplot")
+
+# The attribute as the x axis; the label is going to be on the y axis
+p = 'G2'
+# Scatter plot
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
+
+
