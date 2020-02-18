d34ed65d35a9263abd882d4a9d8d547cfa08817a
Ethan <ethan.chen@covenant.edu>
2020-02-17 23:20:14 -0500

    Implement K Means model

--- tensorEnv/K Means Example.py
+++ tensorEnv/K Means Example.py
@@ -18,5 +18,24 @@ k = 10
 samples, features = data.shape
 
 # Scoring
-# To score the model use a function from sklearn to compute a
+# To score the model use a function from sklearn to compute many
+# different scores from different parts of our model
+def bench_k_means(estimator, name, data):
+    estimator.fit(data)
+    print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, estimator.inertia_,
+             metrics.homogeneity_score(y, estimator.labels_),
+             metrics.completeness_score(y, estimator.labels_),
+             metrics.v_measure_score(y, estimator.labels_),
+             metrics.adjusted_rand_score(y, estimator.labels_),
+             metrics.adjusted_mutual_info_score(y,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean')))
+
+# Training the model
+# Create the model with K Means classifier
+clf = KMeans(n_clusters=k, init="random", n_init=10)
+bench_k_means(clf, "1", data)
+
+# Using Matplotlib to visualize
 
