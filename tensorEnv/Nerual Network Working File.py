490a0ddb7d5f641039da0269c1b5e6f9b869b772
Ethan <ethan.chen@covenant.edu>
2020-02-20 21:47:18 -0500

    Create a Neural Network Model to train the data

--- tensorEnv/Nerual Network Working File.py
+++ tensorEnv/Nerual Network Working File.py
@@ -17,5 +17,20 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images/255.0
-test_images = test_images/255.0
+
+model = keras.Sequential([
+	keras.layers.Flatten(input_shape=(28,28)),
+	keras.layers.Dense(128, activation="relu"),
+	keras.layers.Dense(10, activation="softmax")
+	])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('\nTest accuracy:', test_acc)
+
+# train_images = train_images/255.0
+# test_images = test_images/255.0
